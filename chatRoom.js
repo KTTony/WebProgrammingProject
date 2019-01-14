ba5e0a553a5e9ad7d5899bb9fc4e068b35aa4a19
KTtony <34537745+KTtony@users.noreply.github.com>
2019-01-14 09:49:11 +0800

    回首頁關閉影片V2

--- chatRoom.js
+++ chatRoom.js
@@ -15,9 +15,9 @@ document.querySelector("footer .aboutButton").addEventListener('click',()=>{
     document.querySelector(".aboutUS").style.display = "flex";
     document.querySelector(".backToMain").style.display = "block";
     document.querySelector(".movieWrap").style.display = "none";
-    document.querySelector('body').scrollIntoView({ behavior: 'smooth', block: 'start' });
     var textBox = document.querySelector(".textBox");
     textBox.scrollTop = textBox.scrollHeight;
+    document.querySelector('body').scrollIntoView({ behavior: 'smooth', block: 'start' });
 })
 
 
--- scroll.js
+++ scroll.js
@@ -1,3 +1,55 @@
+const videoOne = document.getElementById("one").querySelector('div #video_view');
+const videoTwo = document.getElementById("two").querySelector('div #video_view');
+const videoThree = document.getElementById("three").querySelector('div #video_view');
+
+console.log(videoOne)
+
+function showVideo(link,video) {
+    // const video = document.querySelector('div #video_view');
+    video.innerHTML = link;
+    document.body.classList.add('no-scroll');
+    video.style.top = window.pageYOffset + 'px';
+    video.classList.remove('hidden');
+}
+function offVideo(video) {
+    document.body.classList.remove('no-scroll');
+    video.innerHTML = "";
+    video.classList.add('hidden');
+}
+
+const toVideoOne = document.querySelector('#one .movie_about .remark .video');
+toVideoOne.addEventListener('click', () => {
+    const link = '<iframe src="https://www.youtube.com/embed/4VrcUVzvRwM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>';
+    showVideo(link,videoOne);
+});
+const toVideoTwo = document.querySelector('#two .movie_about .remark .video');
+toVideoTwo.addEventListener('click', () => {
+    const link = '<iframe src="https://www.youtube.com/embed/N2WCq9cIPEI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>';
+    showVideo(link,videoOne);
+});
+const toVideoThree = document.querySelector('#three .movie_about .remark .video');
+toVideoThree.addEventListener('click', () => {
+    const link = '<iframe src="https://www.youtube.com/embed/WPiJhQgKFX8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>';
+    showVideo(link,videoOne);
+});
+
+
+videoOne.addEventListener('click', ()=>{
+    offVideo(videoOne);
+});
+videoTwo.addEventListener('click', ()=>{
+    offVideo(videoTwo);
+});
+videoThree.addEventListener('click', ()=>{
+    offVideo(videoThree);
+});
+
+
+
+
+
+
+
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     scrollButton();
